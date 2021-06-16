263fb122eb2f3186e69245349e4fc88d2ee81f13
Jeff Greer <greerg@gmail.com>
2021-06-16 11:34:59 -0700

    add bash libraries to find OS/Distro info and change echo colors

---
+++ src/lib/colors.bash
@@ -0,0 +1,10 @@
+function magenta {
+    echo -e '\033[38;2;200;100;2552m'
+}
+function color_off {
+    echo -e '\033[m'
+}
+
+function green {
+    echo -e '\033[38;2;0;255;02m'
+}
---
+++ src/lib/os_information.bash
@@ -0,0 +1,12 @@
+function get_os_name {
+	echo `uname -s | tr "A-Z" "a-z"`
+}
+function get_os_release_codename {
+	echo `lsb_release -c | tr -d \s  | cut -d':' -f2  | tr "A-Z" "a-z"`
+}
+function get_os_relese_name {
+	echo `lsb_release -i | tr -d \s  | cut -d':' -f2  | tr "A-Z" "a-z"`
+}
+function get_os_release_number {
+	echo `lsb_release -r | tr -d \s  | cut -d':' -f2`
+}
