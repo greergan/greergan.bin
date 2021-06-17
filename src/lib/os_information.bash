858864af3bb630938df2bd63a357c3bf1cab18ff
Jeff Greer <greerg@gmail.com>
2021-06-16 17:05:08 -0700

    added alternat syntax to a lsb_release command wrapper

--- src/lib/os_information.bash
+++ src/lib/os_information.bash
@@ -4,8 +4,8 @@ function get_os_name {
 function get_os_release_codename {
 	echo `lsb_release -c | tr -d \s  | cut -d':' -f2  | tr "A-Z" "a-z"`
 }
-function get_os_relese_name {
-	echo `lsb_release -i | tr -d \s  | cut -d':' -f2  | tr "A-Z" "a-z"`
+function get_os_release_name {
+	echo `lsb_release -i | cut -d":" -f2 | sed 's/\s*//' | tr [A-Z] [a-z]`
 }
 function get_os_release_number {
 	echo `lsb_release -r | tr -d \s  | cut -d':' -f2`
